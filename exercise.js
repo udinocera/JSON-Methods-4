a0b0ff906e29bbbd48383e6ac18fd4332533b079
udinocera <udinocera@gmail.com>
2023-09-04 03:12:05 +0200

    fine

---
+++ exercise.js
@@ -0,0 +1,16 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25,
+  toJSON(){
+    return{
+      id: this.id,
+      age: this.age
+    };
+  }
+};
+
+const json = JSON.stringify(person);
+
+console.log(json); // Should return: { id: 1, age: 25 }
